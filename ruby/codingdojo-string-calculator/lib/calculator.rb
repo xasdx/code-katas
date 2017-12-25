9a344d285a1d7b4e6f184089e35a15029416fd1e
_asdx_ <xasdx@tuta.io>
2017-12-25 13:09:24 +0000

    string-calculator / Refactor the parsing of the input string to a separate class

--- ruby/codingdojo-string-calculator/lib/calculator.rb
+++ ruby/codingdojo-string-calculator/lib/calculator.rb
@@ -1,29 +1,20 @@
-class Calculator
+class CalculatorInput
   
-  def add(input)
-    if input.start_with? "//"
-      separator, numbers = parse_custom_separator(input)
-      sum(numbers, separator)
+  attr_reader :numbers
+  
+  def initialize(input)
+    if uses_custom_separator?(input)
+      separator, numbers = parse_custom_separator input
+      @numbers = parse_numbers(numbers, separator)
     else
-      sum input
+      @numbers = parse_numbers input
     end
   end
   
   private
   
-  def sum(str, separator = ",")
-    splitter = Regexp.new('[' + separator + '\n]')
-    
-    numbers = str.split(splitter)
-      .each { |n| raise ArgumentError.new("Invalid use of separators") if n.empty? }
-      .map(&:to_f)
-    
-    if numbers.any? { |n| n < 0 }
-      negatives = numbers.select { |n| n < 0 }
-      raise ArgumentError.new("Negative numbers are not allowed: #{negatives.join(', ')}")
-    end
-      
-    numbers.sum
+  def uses_custom_separator?(input)
+    input.start_with? "//"
   end
   
   def parse_custom_separator(input)
@@ -31,5 +22,30 @@ class Calculator
     separator = input[2, new_line_index - 2]
     numbers = input[new_line_index + 1 .. input.length - 1]
     return separator, numbers
+  end
+  
+  def parse_numbers(str, separator = ",")
+    splitter = Regexp.new('[' + separator + '\n]')
+    return str.split(splitter)
+      .each { |n| raise ArgumentError.new("Invalid use of separators") if n.empty? }
+      .map(&:to_f)
+  end
+end
+
+class Calculator
+  
+  def add(input)
+    numbers = CalculatorInput.new(input).numbers
+    assert_no_negatives numbers
+    return numbers.sum
+  end
+  
+  private
+  
+  def assert_no_negatives(numbers)
+    if numbers.any? { |n| n < 0 }
+      negatives = numbers.select { |n| n < 0 }
+      raise ArgumentError.new("Negative numbers are not allowed: #{negatives.join(', ')}")
+    end
   end
 end
