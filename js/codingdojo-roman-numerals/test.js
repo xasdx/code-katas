7034d4f63e0eff6f23e3f5eb2fd8da60f3e93134
_asdx_ <xasdx@tuta.io>
2017-12-27 08:24:04 +0000

    roman-nums / Handle digits 4 and 9 when converting to roman

--- js/codingdojo-roman-numerals/test.js
+++ js/codingdojo-roman-numerals/test.js
@@ -11,6 +11,8 @@ test("converts 3 to III", t => t.is(toRoman(3), "III"))
 
 test("converts 4 to IV", t => t.is(toRoman(4), "IV"))
 
+test("converts 9 to IX", t => t.is(toRoman(9), "IX"))
+
 test("converts numbers to their one-digit roman equal", t => [
   { decimal: 5, roman: "V" },
   { decimal: 10, roman: "X" },
@@ -25,3 +27,11 @@ test("converts 2678 to a roman number", t => t.is(toRoman(2678), "MMDCLXXVIII"))
 test("rejects numbers > 3000", t => t.throws(() => toRoman(3001), RangeError))
 
 test("converts 3000 to a roman number", t => t.is(toRoman(3000), "MMM"))
+
+test("converts 954", t => t.is(toRoman(954), "CMLIV"))
+
+test("converts 494", t => t.is(toRoman(494), "CDXCIV"))
+
+test("converts 444", t => t.is(toRoman(444), "CDXLIV"))
+
+test("converts 999", t => t.is(toRoman(999), "CMXCIX"))
