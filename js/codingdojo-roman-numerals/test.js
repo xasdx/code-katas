dada1103a1e19fd304951ea4d9781100120f4290
_asdx_ <xasdx@tuta.io>
2017-12-27 08:47:10 +0000

    roman-nums / Refactor test case inputs to an object

--- js/codingdojo-roman-numerals/test.js
+++ js/codingdojo-roman-numerals/test.js
@@ -3,35 +3,47 @@ let underTest = require(".")
 
 let toRoman = underTest.toRoman
 
-test("rejects zero", t => t.throws(() => toRoman(0), RangeError))
+let cases = {
+  zero: { n: 0, r: RangeError },
+  one: { n: 1, r: "I" },
+  three: { n: 3, r: "III" },
+  four: { n: 4, r: "IV" },
+  nine: { n: 9, r: "IX" },
+  threeK: { n: 3000, r: "MMM" },
+  overThreeK: { n: 3001, r: RangeError },
+  digit: [
+    { n: 5, r: "V" },
+    { n: 10, r: "X" },
+    { n: 50, r: "L" },
+    { n: 100, r: "C" },
+    { n: 500, r: "D" },
+    { n: 1000, r: "M" }
+  ],
+  random: [
+    { n: 2678, r: "MMDCLXXVIII" },
+    { n: 954, r: "CMLIV" },
+    { n: 494, r: "CDXCIV" },
+    { n: 444, r: "CDXLIV" },
+    { n: 999, r: "CMXCIX" }
+  ]
+}
 
-test("converts decimal 1 to roman I", t => t.is(toRoman(1), "I"))
+test("rejects zero", t => t.throws(() => toRoman(cases.zero.n), cases.zero.r))
 
-test("converts 3 to III", t => t.is(toRoman(3), "III"))
+test("converts decimal 1 to roman I", t => t.is(toRoman(cases.one.n), cases.one.r))
 
-test("converts 4 to IV", t => t.is(toRoman(4), "IV"))
+test("converts 3 to III", t => t.is(toRoman(cases.three.n), cases.three.r))
 
-test("converts 9 to IX", t => t.is(toRoman(9), "IX"))
+test("converts 4 to IV", t => t.is(toRoman(cases.four.n), cases.four.r))
 
-test("converts numbers to their one-digit roman equal", t => [
-  { decimal: 5, roman: "V" },
-  { decimal: 10, roman: "X" },
-  { decimal: 50, roman: "L" },
-  { decimal: 100, roman: "C" },
-  { decimal: 500, roman: "D" },
-  { decimal: 1000, roman: "M" }
-].forEach(({ decimal, roman }) => t.is(toRoman(decimal), roman)))
+test("converts 9 to IX", t => t.is(toRoman(cases.nine.n), cases.nine.r))
 
-test("converts 2678 to a roman number", t => t.is(toRoman(2678), "MMDCLXXVIII"))
+test("converts numbers to their one-digit roman equal",
+      t => cases.digit.forEach(({ n, r }) => t.is(toRoman(n), r)))
 
-test("rejects numbers > 3000", t => t.throws(() => toRoman(3001), RangeError))
+test("rejects numbers > 3000", t => t.throws(() => toRoman(cases.overThreeK.n), cases.overThreeK.r))
 
-test("converts 3000 to a roman number", t => t.is(toRoman(3000), "MMM"))
+test("converts 3000 to a roman number", t => t.is(toRoman(cases.threeK.n), cases.threeK.r))
 
-test("converts 954", t => t.is(toRoman(954), "CMLIV"))
-
-test("converts 494", t => t.is(toRoman(494), "CDXCIV"))
-
-test("converts 444", t => t.is(toRoman(444), "CDXLIV"))
-
-test("converts 999", t => t.is(toRoman(999), "CMXCIX"))
+test("converts random numbers",
+      t => cases.random.forEach(({ n, r }) => t.is(toRoman(n), r)))
