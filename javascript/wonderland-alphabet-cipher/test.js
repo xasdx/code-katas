9b6ec08d6e27ba59a39327d1b48e5c33730f73f1
_asdx_ <xasdx@tuta.io>
2018-02-11 17:04:19 +0000

    alphabet-cipher / Add a test case on generating an intermediate key

--- javascript/wonderland-alphabet-cipher/test.js
+++ javascript/wonderland-alphabet-cipher/test.js
@@ -1,2 +1,10 @@
 let test = require("ava")
-let underTest = require(".")
+let AlpabetCipher = require(".")
+
+test("generates a string using the keyword to be combined with the plain text", t => {
+  let keyword = "scones"
+  let underTest = new AlpabetCipher(keyword)
+  t.is(underTest.keyword, keyword)
+  let key = underTest.generateKeyFor("meetmebythetree")
+  t.is(key, "sconessconessco")
+})
