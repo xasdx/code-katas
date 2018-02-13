76e2cffad61eb35475597e5c35e2dc2a88b99343
_asdx_ <xasdx@tuta.io>
2018-02-13 15:43:42 +0000

    alphabet-cipher / Implement solution

--- javascript/wonderland-alphabet-cipher/index.js
+++ javascript/wonderland-alphabet-cipher/index.js
@@ -1,3 +1,5 @@
+let alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
+
 module.exports = class AlpabetCipher {
   
   constructor(keyword) {
@@ -10,7 +12,12 @@ module.exports = class AlpabetCipher {
   }
   
   encrypt(plainText) {
+    let plain = plainText.split("")
     let key = this.generateKeyFor(plainText)
-    return "egsgqwtahuiljgs"
+    return key.split("").map((keyChar, index) => {
+      let i = keyChar.charCodeAt(0) - 96
+      let j = plain[index].charCodeAt(0) - 96
+      return alphabet[(i + j - 1) % alphabet.length - 1]
+    }).join("")
   }
 }
