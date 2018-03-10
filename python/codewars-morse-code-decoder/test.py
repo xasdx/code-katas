aeeb6951a6afdc0fc1329b30bfad6185f474a9fe
_asdx_ <xasdx@tuta.io>
2018-03-10 07:04:43 +0000

    morse-code / Implement a simple code decoder

--- python/codewars-morse-code-decoder/test.py
+++ python/codewars-morse-code-decoder/test.py
@@ -5,6 +5,12 @@ class MorseDecoderTest(unittest.TestCase):
   def test_decodes_a_character(self):
     decoder = MorseDecoder()
     self.assertEqual(decoder.decode("...."), "H")
+  def test_decodes_a_word(self):
+    decoder = MorseDecoder()
+    self.assertEqual(decoder.decode(".... . -.--"), "HEY")
+  def test_decodes_a_sentence(self):
+    decoder = MorseDecoder()
+    self.assertEqual(decoder.decode(".... . -.--   .--- ..- -.. ."), "HEY JUDE")
 
 if __name__ == "__main__":
   unittest.main()
